4daedae8471a868142aa056bf36e6bca7829ab20
Se7enB2st <ethanhe1992@gmail.com>
2016-07-24 03:46:58 +0000

    fix uninitialized constant error

--- todos/spec/features/user_completes_todo_spec.rb
+++ todos/spec/features/user_completes_todo_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
-feature "User complete todo" do
+
+feature "User completes todo" do
   scenario "successfully" do
     sign_in
 
@@ -8,7 +9,7 @@ feature "User complete todo" do
     fill_in "Title", with: "Working Out"
     click_on "Submit"
 
-    click_on "Completed!"
+    click_on "Complete!"
 
     expect(page).to have_css '.todos li.completed', text: 'Working Out'
   end
